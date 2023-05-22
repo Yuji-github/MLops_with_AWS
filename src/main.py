c8997dedbc1489b73beb7a2c6e4917cca1fc66cb
Yuji <81yishikawa@gmail.com>
2023-05-22 18:28:01 +1000

    test

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name :     str):
+def print_hi(name :           str):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
