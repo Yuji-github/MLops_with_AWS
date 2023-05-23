904d8b013858d988bcdf52ae15b205a4dbf4e64a
Ayami <a.nonaka.116@gmail.com>
2023-05-23 22:01:42 +1000

    [15] changed code

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,10 +1,14 @@
 import unittest
+from src.main import print_hi
 
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, True)  # add assertion here
 
+    def test_print_hi_with_string(self):
+        self.assertEqual("Hi, good", print_hi("good"))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
