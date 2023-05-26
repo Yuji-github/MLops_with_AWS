7a841c35a01a8e25dadeca5a71673330e1cf9239
Yuji <81yishikawa@gmail.com>
2023-05-26 17:54:38 +1000

    [20] imported the data set

--- src/main.py
+++ src/main.py
@@ -1,5 +1,17 @@
-# entry point of the code
+"""Entry point to run the code
+Step 1:
+    EDA (Exploratory Data Analysis)
+Step 2:
+    Create a model : Save the artifacts on S3
+Step 3:
+    Evaluate the model : Save the artifacts on S3
+Step 4:
+    Deploy the model : Save the artifacts on Sagemaker
+Step 5:
+    Test deployment
+"""
 from settings import URI
+import pandas as pd
 
 if __name__ == "__main__":
-    print(print_hi("PyCharm"))
+    df = pd.read_csv("Stars.csv")
