7b68a34a04127246e5eb1e376752c899c8caaa27
Ayami <a.nonaka.116@gmail.com>
2023-06-13 15:41:05 +1000

    [S3_download(2)] create registery.py

---
+++ src/mlflow_s3_download.sh
@@ -0,0 +1,2 @@
+echo please, register it.
+python registery_and_deployment.py
--- src/registery_and_deployment.py
+++ src/registery_and_deployment.py
@@ -1,18 +1,25 @@
 import boto3
-import botocore
 import os
 
 
-def client():
+def download_s3(BUCKET_NAME, BUCKET_PREFIX):
     """Create a low-level service client by name using the default session.
 
     :param service_name: The name of a service, e.g. 's3' or 'ec2'. You
             can get a list of available services via
     """
 
-    BUCKET_NAME = "mlflow-s3-bucket-1"
-    BUCKET_PREFIX = "584097635432506948/0153458fb9fa45c7b560f9f9fbbae162/artifacts/GradientBoost"
+    # BUCKET_NAME = "mlflow-s3-bucket-1"
+    # BUCKET_PREFIX = "584097635432506948/0153458fb9fa45c7b560f9f9fbbae162/artifacts/GradientBoost"
 
+    if not os.path.exists(File_name):
+        os.mkdir(File_name)
+        os.chdir(File_name)
+        print("Folder %s created!" % File_name)
+    else:
+        os.chdir(File_name)
+
+    cur_path = os.getcwd()
     client_s3 = boto3.client("s3")
     list_s3 = client_s3.list_objects_v2(
         Bucket=BUCKET_NAME, Prefix=BUCKET_PREFIX
@@ -21,16 +28,23 @@ def client():
     for obj in list_s3["Contents"]:
 
         try:
-            filename = obj["Key"].rsplit("/", 1)[
-                1
-            ]  # If Filename --> /local/path/file.txt this step are get only this file "file.txt"
+            filename = obj["Key"].rsplit("/", 1)[1]
         except IndexError:
             filename = obj["Key"]
 
-        localfilename = os.path.join("C:\\Users\\User\\PycharmProjects\\MLops_with_AWS\\download\\", filename)
+        localfilename = os.path.join(cur_path, filename)
         client_s3.download_file(BUCKET_NAME, obj["Key"], localfilename)
-    client_s3.close()
 
 
 if __name__ == "__main__":
-    client()
+    BUCKET_NAME = input("Enter bucket name:")
+    BUCKET_PREFIX = input("Enter s3 folder/files:")
+    File_name = input("Enter the download files:")
+    if not os.path.exists(File_name):
+        os.mkdir(File_name)
+        os.chdir(File_name)
+        print("Folder %s created!" % File_name)
+    else:
+        os.chdir(File_name)
+
+    download_s3(BUCKET_NAME, BUCKET_PREFIX)
