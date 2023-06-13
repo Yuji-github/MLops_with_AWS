ad1a31ea908d5d167b53f8a7a81395dbcf9890b1
Ayami <a.nonaka.116@gmail.com>
2023-06-13 21:48:33 +1000

    [S3_download(3)] changed code

--- src/mlflow_s3_download.sh
+++
@@ -1,2 +0,0 @@
-echo please, register it.
-python registery_and_deployment.py
--- src/registery_and_deployment.py
+++ src/registery_and_deployment.py
@@ -1,28 +1,57 @@
 import boto3
 import os
+import argparse
+from urllib.parse import urlparse
+from typing import Tuple
 
 
-def download_s3(BUCKET_NAME, BUCKET_PREFIX):
-    """Create a low-level service client by name using the default session.
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_folder_name_s3", type=str, default=None, help="folder path for s3")
+    parser.add_argument("--folder", type=str, default="download", help="saving files to the folder from s3")
 
-    :param service_name: The name of a service, e.g. 's3' or 'ec2'. You
-            can get a list of available services via
+    return parser.parse_args()
+
+
+def download_s3(model_folder_name_s3: str, folder: str):
+    """Downloading files from s3 bucket folder
+
+    :param model_folder_name_s3:
+    :param folder:
+    :return:
+
+    e.g.
+    model_folder_name_s3 = s3://s3-bucket-name/1234/5678/artifacts/GradientBoost
+    folder = download
     """
 
-    # BUCKET_NAME = "mlflow-s3-bucket-1"
-    # BUCKET_PREFIX = "584097635432506948/0153458fb9fa45c7b560f9f9fbbae162/artifacts/GradientBoost"
+    def separate_path(model_folder_name_s3: str) -> Tuple[str, str]:
+        """Separating into bucket and path
 
-    if not os.path.exists(File_name):
-        os.mkdir(File_name)
-        os.chdir(File_name)
-        print("Folder %s created!" % File_name)
+        :param model_folder_name_s3:
+             s3://s3-bucket-name/1234/5678/artifacts/GradientBoost
+
+        :return bucket_name,bucket_prefix:
+            s3-bucket-name, /1234/5678/artifacts/GradientBoost
+        """
+        parse_object = urlparse(model_folder_name_s3)
+        bucket_name = parse_object.netloc
+        bucket_prefix = parse_object.path[1:]
+        return bucket_name, bucket_prefix
+
+    bucket_name, bucket_prefix = separate_path(model_folder_name_s3)
+
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+        os.chdir(folder)
+        print("Folder %s created!" % folder)
     else:
-        os.chdir(File_name)
+        os.chdir(folder)
 
     cur_path = os.getcwd()
-    client_s3 = boto3.client("s3")
+    client_s3 = boto3.client("s3")  # boto3.client(service_name): The name of a service, e.g. 's3' or 'ec2'.
     list_s3 = client_s3.list_objects_v2(
-        Bucket=BUCKET_NAME, Prefix=BUCKET_PREFIX
+        Bucket=bucket_name, Prefix=bucket_prefix
     )  # Bucket is s3 bucket name. Prefix is Limits the response to keys that begin with the specified prefix.
 
     for obj in list_s3["Contents"]:
@@ -33,18 +62,9 @@ def download_s3(BUCKET_NAME, BUCKET_PREFIX):
             filename = obj["Key"]
 
         localfilename = os.path.join(cur_path, filename)
-        client_s3.download_file(BUCKET_NAME, obj["Key"], localfilename)
+        client_s3.download_file(bucket_name, obj["Key"], localfilename)
 
 
 if __name__ == "__main__":
-    BUCKET_NAME = input("Enter bucket name:")
-    BUCKET_PREFIX = input("Enter s3 folder/files:")
-    File_name = input("Enter the download files:")
-    if not os.path.exists(File_name):
-        os.mkdir(File_name)
-        os.chdir(File_name)
-        print("Folder %s created!" % File_name)
-    else:
-        os.chdir(File_name)
-
-    download_s3(BUCKET_NAME, BUCKET_PREFIX)
+    args = parse_args()
+    download_s3(args.model_folder_name_s3, args.folder)
