8a3d8d7507383f4cf14c02db83a3658208b0fbad
Yuji <81yishikawa@gmail.com>
2023-06-18 21:21:00 +1000

    [2] modified the code

--- src/connect_to_sagemaker.py
+++ src/connect_to_sagemaker.py
@@ -5,6 +5,7 @@ import json
 import boto3
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from eda import EDA
 
 global app_name
 global region
@@ -28,6 +29,7 @@ def query_endpoint(app_name: str, input_json):
         Body=input_json,
         ContentType="application/json; format=pandas-split",
     )
+    print(response)
     preds = response["Body"].read().decode("ascii")
     preds = json.loads(preds)
     print("Received response: {}".format(preds))
@@ -40,14 +42,12 @@ print("Application status is: {}".format(check_status(app_name)))
 # Prepare data to give for predictions
 df = pd.read_csv("src/Stars.csv")
 
-x, y = df.iloc[:, :-1], df.iloc[:, -1]
-sc = StandardScaler()
-
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
-x_train = sc.fit_transform(x_train)
-x_test = sc.transform(x_test)
+this_eda = EDA(df)
+this_eda._replace_df_cols()
+x_train, x_test, y_train, y_test = this_eda._spread_df()
 
 
 # ## create test data and make inference from enpoint
-# query_input = pd.DataFrame(x_test).iloc[[3]].to_json(orient="split")
+query_input = pd.DataFrame(x_test).iloc[[3]].to_json(orient="split")
+print(query_input)
 # prediction = query_endpoint(app_name=app_name, input_json=query_input)
