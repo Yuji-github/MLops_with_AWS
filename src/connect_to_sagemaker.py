4186d27b76e06d2acebe8c3ac6f0bac4a18b28db
Yuji <81yishikawa@gmail.com>
2023-06-19 00:19:21 +1000

    [3] modified the code and this works

--- src/connect_to_sagemaker.py
+++ src/connect_to_sagemaker.py
@@ -2,61 +2,53 @@ import pandas as pd
 import json
 import boto3
 from eda import EDA
-
-global app_name
-global region
-
-app_name = "sagemaker-model"  # model name on sagemaker
-region = "ap-southeast-2"
+import argparse
+import random
 
 
-def check_status(app_name: str):
+def check_status(endpoint_name: str, region: str):
     sage_client = boto3.client("sagemaker", region_name=region)
-    endpoint_description = sage_client.describe_endpoint(EndpointName=app_name)
+    endpoint_description = sage_client.describe_endpoint(EndpointName=endpoint_name)
     endpoint_status = endpoint_description["EndpointStatus"]
     return endpoint_status
 
 
-# def query_endpoint(app_name: str, input_json):
-#     # client = boto3.session.Session().client("sagemaker-runtime", region)
-#     client = boto3.client('sagemaker-runtime')
-#     response = client.invoke_endpoint(
-#         EndpointName=app_name,
-#         Body=input_json,
-#         ContentType="application/json; format=pandas-split",
-#     )
-#     print(response)
-#     preds = response["Body"].read().decode("ascii")
-#     preds = json.loads(preds)
-#     print("Received response: {}".format(preds))
-#     return None
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--endpoint_name", type=str, default=None, help="EndPoint Name on Sagemaker")
+    parser.add_argument("--region", type=str, default="ap-southeast-2", help="Region where in Service")
+
+    return parser.parse_args()
 
 
-## check endpoint status
-print("Application status is: {}".format(check_status(app_name)))
+if __name__ == "__main__":
+    args = parse_args()
 
-# Prepare data to give for predictions
-df = pd.read_csv("src/Stars.csv")
+    # check endpoint status
+    if check_status(args.endpoint_name, args.region) == "InService":
 
-this_eda = EDA(df)
-this_eda._replace_df_cols()
-x_train, x_test, y_train, y_test = this_eda._spread_df()
+        # Prepare data to give for predictions
+        df = pd.read_csv("src/Stars.csv")
+        this_eda = EDA(df)
+        this_eda._replace_df_cols()
+        x_train, x_test, y_train, y_test = this_eda._spread_df()
 
-actual = y_test.iloc[3]
-# ## create test data and make inference from enpoint
-data = {"instances": [x_test[3].tolist()]}  # need to be 2D array
+        # selecting test data
+        test_idx = random.randrange(len(x_test))  # getting random number from the range
+        data = {"instances": [x_test[test_idx].tolist()]}  # need to be 2D array
 
-print(data)
+        # connecting Sagemaker
+        client = boto3.client("sagemaker-runtime")
 
-client = boto3.client("sagemaker-runtime")
+        # Body and ContentType must be same
+        # invoke_endpoint predicts the results with given values (Body)
+        # reference: https://medium.com/ml-bytes/how-to-make-predictions-against-a-sagemaker-endpoint-using-tensorflow-serving-8b423b9b316a
 
-# Body and ContentType must be same
-# invoke_endpoint predicts the results with given values (Body)
-# reference: https://medium.com/ml-bytes/how-to-make-predictions-against-a-sagemaker-endpoint-using-tensorflow-serving-8b423b9b316a
+        response = client.invoke_endpoint(
+            EndpointName=args.endpoint_name, Body=json.dumps(data), ContentType="application/json"
+        )
 
-response = client.invoke_endpoint(EndpointName=app_name, Body=json.dumps(data), ContentType="application/json")
-
-# decoding (converting) the response
-preds = response["Body"].read().decode("ascii")
-preds = json.loads(preds)
-print(f"Received response: {preds}, actual {actual}")
+        # decoding (converting) the response
+        preds = response["Body"].read().decode("ascii")
+        preds = json.loads(preds)
+        print(f"Received response: {preds}, actual {y_test.iloc[test_idx]}")
